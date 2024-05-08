59f7bfe0bca588d0a642dbfb2998c7339cd75630
naaltunian <naaltunian@icloud.com>
2024-05-08 11:09:12 -0400

    solution

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // all code for this lab to be completed in this file
+
+const currentTemperature = 15
+
+if (currentTemperature > 32) {
+  console.log('It is not freezing.')
+} else {
+  console.log('It is freezing')
+}
+
+const myAge = 30
+const yourAge = 36
+
+if (myAge === yourAge) {
+  console.log('We are the same age.')
+} else if (myAge > yourAge) {
+  console.log('I am older than you.')
+} else {
+  console.log()
+}
+
+const isLoggedIn = true
+
+if (isLoggedIn) {
+  console.log('Welcome back!')
+} else {
+  console.log('Please log in to continue...')
+}
