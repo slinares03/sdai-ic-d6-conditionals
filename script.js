e32859169b0481d780556ab0466442880cd28ad1
Salvador Linares <35157273+slinares03@users.noreply.github.com>
2024-11-08 05:17:53 +0000

    Added JS

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // all code for this lab to be completed in this file
+//const currentTemperature = 40
+//if (currentTemperature > 32) {
+    //console.log("It is not freezing.")
+//} else {
+    //console.log("It is freezing")
+//}
+const myAge = 21
+const yourAge = 22
+
+if (myAge === yourAge) {
+    console.log("We are the same age")
+} else if (myAge > yourAge) {
+    console.log("I am older than you")
+} else {
+    console.log("You are older than me")
+}
